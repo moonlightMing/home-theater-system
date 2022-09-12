f221d34723f93f204d8cdea3273892394d66a357
moonlightming <605365071@qq.com>
2022-09-12 19:21:46 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # home-theater-system
+
+集成家庭电影系统套件，开箱即用
+
+## 集成
+- NAS-Tools
+- Jellyfin
+- qBittorrent
+- Jackett
---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /srv/{jellyfin,nas-tools,qbittorrent,jackett}
+
+mkdir -p /srv/jellyfin/{cache,config}
+mkdir -p /srv/nas-tools/config
+mkdir -p /srv/qbittorrent/config
+mkdir -p /srv/jackett/{config,downloads}
