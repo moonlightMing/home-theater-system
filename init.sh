5e9962a0295896b8e8e518eaaf8e69a72b71bdaf
moonlightming <605365071@qq.com>
2022-09-14 21:57:04 +0800

    update

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir -p /srv/{jellyfin,nas-tools,qbittorrent,jackett}
+mkdir -p /srv/jellyfin/cache
+mkdir -p /srv/jellyfin/config
 
-mkdir -p /srv/jellyfin/{cache,config}
 mkdir -p /srv/nas-tools/config
+
 mkdir -p /srv/qbittorrent/config
-mkdir -p /srv/jackett/{config,downloads}
+
+mkdir -p /srv/jackett/config
+mkdir -p /srv/jackett/downloads
